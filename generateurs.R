9680255b1240f4c14e4925cc3aabbda647457638
Sophie Crowley <crowleso@tcd.ie>
2020-05-05 14:20:47 +0100

    1st ver of Randu and Standard Minimal

--- generateurs.R
+++ generateurs.R
@@ -1,3 +1,25 @@
+RANDU = function(k, graine)
+{
+  x <-matrix(nrow=k, ncol=1)
+  x[1] = graine
+  for(i in 2:k)
+  {
+    x[i] = (x[i-1] * 65539) %% (2^31)-1
+  }  
+  return(x)
+}
+
+StandardMinimal = function(k,graine)
+{
+  x <-  matrix(nrow=k, ncol=1)
+  x[1] = graine
+  for(i in 2:k)
+  {
+    x[i] = (x[i-1] * 16807) %% 2^31
+  }
+  return(x)
+}
+
 VonNeumann <- function(n, p=1, graine)
 {
   x <-  rep(graine,n*p+1)
--- seance1.R
+++ seance1.R
@@ -6,6 +6,8 @@ source('generateurs.R')
 
 sVN <- 9721
 sMT <- 2504
+sR <- 999
+sSM <- 999
 Nsimu <- 1000
 Nrepet <- 20
 
@@ -16,14 +18,20 @@ Nrepet <- 20
 
 vn <- VonNeumann(Nsimu,Nrepet,sVN)
 mt <- MersenneTwister(Nsimu,Nrepet,sMT)
+randu = RANDU(Nsimu, sR)
+sm = StandardMinimal(Nsimu, sSM)
 
 par(mfrow=c(1,2))
 hist(mt[,1],xlab='',main='Mersenne Twister')
 hist(vn[,1],xlab='',main='Von Neumann')
+hist(randu[,1],xlab='',main='RANDU')
+hist(sm[,1],xlab='',main='Standard Minimal')
 
 par(mfrow=c(1,2))
 plot(mt[1:(Nsimu-1),1],mt[2:Nsimu,1],xlab='MT(i)', ylab='MT(i+1)', main='Mersenne Twister')
 plot(vn[1:(Nsimu-1),1],vn[2:Nsimu,1],xlab='VN(i)', ylab='VN(i+1)', main='Von Neumann')
+plot(randu[1:(sR-1),1],vn[2:sR,1],xlab='RANDU(i)', ylab='RANDU(i+1)', main='RANDU')
+plot(sm[1:(sSM-1),1],mt[2:sSM,1],xlab='SM(i)', ylab='SM(i+1)', main='Standard Minimal')
 
 # Sequence de bits pour les tests
 (bit_mt <- binary(mt[1,1]))
