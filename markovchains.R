45cbb111271cd408b0f36d07f47cde3b5553fe83
Sophie Crowley <crowleso@tcd.ie>
2020-05-12 16:32:08 +0100

    Update markovchains.R

--- markovchains.R
+++ markovchains.R
@@ -1,6 +1,11 @@
 library(randtoolbox)
 source('functions.R')
 
-queue <- FileMM1(20,15, 12)
+queue <- FileMM1(8,15,12)
 results <-MM1Evolution(queue)
-plot(results[[1]],results[[2]],'s')
+results[[1]]
+results[[2]]
+
+plot(results[[1]], results[[2]])
+plot(results[[1]], results[[2]], 's')
+
